178d0fefbf03ace290df7686f28a77dfa2561398
1x2kb <natemeyer@protonmail.com>
2024-03-23 16:23:54 -0500

    Calls code from main

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,61 @@
-use std::io::{stdout, Write};
-
 use clap::Parser;
 use rand::Rng;
 
 #[derive(Debug, Parser)]
 #[clap(author, version, about, long_about = None)]
-struct Args {
+struct RngGenerator {
     #[clap(short, long, default_value_t = 0)]
-    min: u64,
+    pub min: u64,
     #[clap(short('x'), long, default_value_t = 100)]
-    max: u64,
+    pub max: u64,
     #[clap(default_value_t = 1)]
-    numbers: usize,
+    pub numbers: usize,
+}
+
+impl GenerateNumbers for RngGenerator {
+    fn generate_numbers(num: usize, min: u64, max: u64) -> Vec<u64> {
+        let mut rng = rand::thread_rng();
+        (0..num)
+            .into_iter()
+            .map(|_| rng.gen_range(min..=max))
+            .collect()
+    }
+}
+
+impl CheckForErrors for RngGenerator {
+    fn check_for_errors(num: usize, min: u64, max: u64) -> Result<(), NumberError> {
+        if min > max {
+            return Err(NumberError::MinGreaterThanMax);
+        }
+        if num == 0 {
+            return Err(NumberError::NumbersNotValid);
+        }
+
+        Ok(())
+    }
+}
+
+impl Execute for RngGenerator
+where
+    RngGenerator: GenerateNumbers + CheckForErrors,
+{
+    fn execute(&self) -> Result<Vec<u64>, NumberError> {
+        let (numbers, min, max) = (self.numbers, self.min, self.max);
+        RngGenerator::check_for_errors(numbers, min, max)?;
+        Ok(RngGenerator::generate_numbers(numbers, min, max))
+    }
+}
+
+trait GenerateNumbers {
+    fn generate_numbers(num: usize, min: u64, max: u64) -> Vec<u64>;
+}
+
+trait CheckForErrors {
+    fn check_for_errors(num: usize, min: u64, max: u64) -> Result<(), NumberError>;
+}
+
+trait Execute {
+    fn execute(&self) -> Result<Vec<u64>, NumberError>;
 }
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
@@ -21,25 +65,22 @@ enum NumberError {
 }
 
 fn main() {
-    let args = Args::parse();
-}
+    let rng_generator = RngGenerator::parse();
 
-fn check_for_errors(num: usize, min: u64, max: u64) -> Result<(), NumberError> {
-    if min > max {
-        return Err(NumberError::MinGreaterThanMax);
-    }
-    if num == 0 {
-        return Err(NumberError::NumbersNotValid);
-    }
-    Ok(())
-}
+    let numbers = match rng_generator.execute() {
+        Ok(numbers) => numbers,
+        Err(error) => match error {
+            NumberError::MinGreaterThanMax => panic!(
+                "Min ({}) cannot be greater than Max ({})",
+                rng_generator.min, rng_generator.max
+            ),
+            NumberError::NumbersNotValid => {
+                panic!("Numbers ({}) cannot be 0", rng_generator.numbers)
+            }
+        },
+    };
 
-fn generate_numbers(num: usize, min: u64, max: u64) -> Vec<u64> {
-    let mut rng = rand::thread_rng();
-    (0..num)
-        .into_iter()
-        .map(|_| rng.gen_range(min..=max))
-        .collect()
+    numbers.into_iter().for_each(|num| println!("{num}"));
 }
 
 #[cfg(test)]
@@ -51,7 +92,7 @@ mod generate_numbers_should {
         let numbers = 10;
 
         let (min, max) = (0, 100);
-        let generated = generate_numbers(numbers, min, max);
+        let generated = RngGenerator::generate_numbers(numbers, min, max);
         assert_eq!(generated.len(), numbers);
     }
 
@@ -60,7 +101,7 @@ mod generate_numbers_should {
         let numbers = 10;
 
         let (min, max) = (10, 15);
-        let generated = generate_numbers(numbers, min, max);
+        let generated = RngGenerator::generate_numbers(numbers, min, max);
         assert!(
             generated.into_iter().all(|num| num >= min && num <= max),
             "Number generated was not between {} and {}",
@@ -80,7 +121,7 @@ mod check_for_errors_should {
         let max = 5;
 
         assert_eq!(
-            check_for_errors(10, min, max),
+            RngGenerator::check_for_errors(10, min, max),
             Err(NumberError::MinGreaterThanMax)
         );
     }
@@ -91,8 +132,55 @@ mod check_for_errors_should {
         let max = 100;
 
         assert_eq!(
-            check_for_errors(0, min, max),
+            RngGenerator::check_for_errors(0, min, max),
             Err(NumberError::NumbersNotValid)
         );
+    }
+}
+
+#[cfg(test)]
+mod execute_should {
+    use super::*;
+
+    #[test]
+    fn return_generated_numbers() {
+        let min = 0;
+        let max = 100;
+
+        let numbers = 100;
+        let rng_generator = RngGenerator { min, max, numbers };
+        let generated = rng_generator.execute().unwrap();
+
+        assert_eq!(generated.len(), numbers);
+        assert!(
+            generated.into_iter().all(|num| num >= min && num <= max),
+            "Number generated was not between {} and {}",
+            min,
+            max
+        );
+    }
+
+    #[test]
+    fn return_error_if_min_greater_than_max() {
+        let min = 10;
+        let max = 5;
+
+        let numbers = 100;
+
+        let rng_generator = RngGenerator { min, max, numbers };
+
+        assert_eq!(rng_generator.execute(), Err(NumberError::MinGreaterThanMax));
+    }
+
+    #[test]
+    fn return_error_if_numbers_is_zero() {
+        let min = 0;
+        let max = 100;
+
+        let numbers = 0;
+
+        let rng_generator = RngGenerator { min, max, numbers };
+
+        assert_eq!(rng_generator.execute(), Err(NumberError::NumbersNotValid));
     }
 }
